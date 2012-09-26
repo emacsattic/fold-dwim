d6a69c57c17970057947db76c345427d160860c9
Peter Heslin <p.j.heslin@dur.ac.uk>
2012-09-26 12:36:14 +0200

    updates

--- fold-dwim.el
+++ fold-dwim.el
@@ -3,7 +3,7 @@
 ;; Copyright (C) 2004 P J Heslin
 ;;
 ;; Author: Peter Heslin <p.j.heslin@dur.ac.uk>
-;; URL: http://www.dur.ac.uk/p.j.heslin/emacs/download/fold-dwim.el
+;; URL: http://www.dur.ac.uk/p.j.heslin/Software/Emacs
 ;; Version: 1.2
 ;;
 ;; This program is free software; you can redistribute it and/or modify
